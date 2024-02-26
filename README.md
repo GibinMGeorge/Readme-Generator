81b2ce6c464c8ab815affe63d856e6a5f9b35f85
Gibin <gibinmgeorge2001@gmail.com>
2024-02-25 18:52:26 -0600

    successfully completed the challenge criterias

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# Readme-Generator
+
+# ReadME Generator
+
+## Description
+This will generate a readme for the project
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+nothing much for now
+
+## Usage
+nothing much, just use carefullyyyyy
+
+## License
+This project is licensed under the None License.
+
+## Contributing
+nope
+
+## Tests
+nothing to test......
+
+## Questions
+For additional questions, contact [gmg18gmg](https://github.com/gmg18gmg) or email gmg18@gmg.com.
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-// TODO: Include packages needed for this application
-const fs = require('fs');
-const inquirer = require('inquirer');
-// TODO: Create an array of questions for user input
+import fs from 'fs';
+import inquirer from 'inquirer';
+
+// Array of questions for user input
 const questions = [
     {
         type: 'input',
@@ -16,7 +16,7 @@ const questions = [
     {
         type: 'input',
         name: 'installation',
-        message: 'Enter installation instructions:'
+        message: 'Enter any installation instructions:'
     },
     {
         type: 'input',
@@ -52,7 +52,6 @@ const questions = [
 ];
 
 // Function to generate README content
-
 function generateReadme(answers) {
     // Generate the content of README.md using the provided answers
     // Return the generated content
@@ -101,18 +100,7 @@ function writeToFile(fileName, data) {
     });
 }
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, err => {
-        if (err) {
-            console.error('Error writing to README.md:', err);
-        } else {
-            console.log('README.md successfully created!');
-        }
-    });
-}
-
-// TODO: Create a function to initialize app
+// Function to initialize app
 function init() {
     inquirer
         .prompt(questions)
